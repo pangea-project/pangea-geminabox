d64e631b176674d5ac99962c3f9b0d219951d293
Harald Sitter <sitter@kde.org>
2017-04-11 13:17:20 +0200

    add vast amounts of code to ensure high quality uploads

--- build_gem.rb
+++ build_gem.rb
@@ -19,6 +19,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 require 'fileutils'
+require 'rubygems/package'
 require 'tmpdir'
 require 'yaml'
 
@@ -69,6 +70,36 @@ spec.mangle!
 # we want the gem to be. BUT, shitty gemspecs that do not resolve paths
 # to their absolute variant will then fail to find assets.
 system('gem', 'build', spec.new_path, '-V', chdir: dir) || raise
+
+# The version of our gem is retained across builds and only changes to new
+# commits. To prevent us from uploading an already existing version we'll
+# attempt to fetch our gem. If we already uploaded the thing we'll have 1
+# gem and be able to exit.
+gem_files = Dir.glob("#{dir}/*.gem")
+raise "Too many gems! #{gemfiles}" unless gem_files.size == 1
+gem_file = gem_files.fetch(0)
+gem_spec = Gem::Package.new(gem_file).spec
+system('gem', 'fetch', '--clear-sources', '--source',
+       'https://gem.cache.pangea.pub', gem_spec.name)
+gem_files = Dir.glob("#{dir}/*.gem")
+if gem_files.size == 1
+  puts "Gem already exists in the box #{gem_files}"
+  exit
+end
+
+# If we have multiple gems now, we can do some quality assertations.
+# Should't have more than 2 now.
+raise "Fetched too many gems #{gem_files}" if gem_files.size > 2
+# If we delete our new one we should be left with one.
+gem_files.delete(gem_file)
+raise "Failed to delete new gem #{gem_files}" if gem_files.size > 1
+# Our _new_ version should be greater than the fetched one.
+other_spec = Gem::Package.new(gem_files.fetch(0)).spec
+if gem_spec.version < other_spec
+  raise "Our new version is older than the one in the box!!!\n" \
+        "#{gem_spec} vs. #{other_spec}"
+end
+
 # .gem/credentials controls API_KEY used here.
-system('gem', 'push', Dir.glob("#{dir}/*.gem").fetch(0),
+system('gem', 'push', gem_file,
        '--host', 'https://gem.cache.pangea.pub') || raise
